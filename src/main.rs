2506c5868d0890394890f9058a1da1c1659af8e5
fastbyt3 <aldeerayan@gmail.com>
2023-10-19 22:29:17 +0530

    Fifth stage: Parse and return User-Agent

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,85 @@
-use std::{net::{TcpListener, TcpStream}, io::{Write, Read}};
+#![allow(dead_code)]
+use std::{net::{TcpListener, TcpStream}, io::{Write, Read}, iter::Map, str::FromStr, collections::HashMap};
 use std::str;
+
+enum HttpMethod {
+    GET
+}
+
+impl FromStr for HttpMethod {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "GET" => Ok(HttpMethod::GET),
+            _ => Err(format!("Unexpected HTTP method -> {}", s))
+        }
+    }
+}
+
+struct RequestStartLine {
+    method: HttpMethod,
+    path: String,
+    version: String,
+}
+
+impl RequestStartLine {
+    fn construct(s: &str) -> RequestStartLine {
+        let mut start_line_parts = s.split_whitespace();
+        let method = start_line_parts.next()
+            .unwrap()
+            .parse::<HttpMethod>()
+            .unwrap();
+        let path = start_line_parts.next().unwrap().to_string();
+        let version = start_line_parts.next().unwrap().to_string();
+
+        RequestStartLine { method, path, version }
+    }
+}
+
+struct Request {
+    start_line: RequestStartLine,
+    headers: HashMap<String, String>,
+}
+
+impl FromStr for Request {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut lines = s.split("\r\n");
+
+        let start_line = RequestStartLine::construct(lines.next().unwrap());
+
+        let mut headers: HashMap<String, String> = HashMap::new();
+        for line in lines {
+            if line.len() == 0 {
+                break;
+            }
+            if let Some((key, value)) = line.split_once(':') {
+                headers.insert(key.to_string(), value.trim().to_string());
+            }
+        }
+
+        Ok(Request { start_line, headers })
+    }
+}
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buf = [0; 1024];
-    stream.read(&mut buf).unwrap();
-    let request = str::from_utf8(&buf).expect("Unable to parse request as &str");
-    println!("{}", request);
-    let path = request.split_whitespace().nth(1).expect("PATH was not found at specified position");
-    println!("{}", path);
+    stream.read(&mut buf).expect("Error reading stream into a buffer");
+    let request = str::from_utf8(&buf).expect("Unable to parse request as &str").parse::<Request>().expect("Unable to parse Request");
 
-    let response: String = match path {
+    let response: String = match request.start_line.path.as_str() {
         "/" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
-        _ if path.starts_with("/echo/") => {
-            let echo = path.strip_prefix("/echo/").expect("No content to echo.... nothing beyond /echo/");
+        _ if request.start_line.path.starts_with("/echo/") => {
+            let echo = request.start_line.path.strip_prefix("/echo/").unwrap();
             println!("{}", echo);
             format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", echo.len(), echo)
         },
+        "/user-agent" => {
+            let ua = request.headers.get("User-Agent").expect("Couldnt find User-Agent key in headers map");
+            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", ua.len(), ua)
+        }
         _ => "HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string()
     };
 
