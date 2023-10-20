59027f6811883b5b5c054b80eb8f243fe86964df
fastbyt3 <aldeerayan@gmail.com>
2023-10-20 22:58:32 +0530

    Eighth stage: Write data to file

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ use std::{
     thread,
 };
 
+#[derive(Debug, PartialEq)]
 enum HttpMethod {
     GET,
+    POST,
 }
 
 impl FromStr for HttpMethod {
@@ -18,6 +20,7 @@ impl FromStr for HttpMethod {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "GET" => Ok(HttpMethod::GET),
+            "POST" => Ok(HttpMethod::POST),
             _ => Err(format!("Unexpected HTTP method -> {}", s)),
         }
     }
@@ -51,6 +54,7 @@ impl RequestStartLine {
 struct Request {
     start_line: RequestStartLine,
     headers: HashMap<String, String>,
+    body: String,
 }
 
 impl FromStr for Request {
@@ -62,7 +66,8 @@ impl FromStr for Request {
         let start_line = RequestStartLine::construct(lines.next().unwrap());
 
         let mut headers: HashMap<String, String> = HashMap::new();
-        for line in lines {
+
+        for line in lines.by_ref() {
             if line.len() == 0 {
                 break;
             }
@@ -71,14 +76,26 @@ impl FromStr for Request {
             }
         }
 
+        let mut other_parts: Vec<&str> = vec![];
+        for remaining_lines in lines {
+            other_parts.push(remaining_lines);
+        }
+        let body = other_parts.join("");
+        println!("Body: {}", body);
+
         Ok(Request {
             start_line,
             headers,
+            body,
         })
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    let ok_200 = "HTTP/1.1 200 OK\r\n";
+    let ok_201 = "HTTP/1.1 201 OK\r\n";
+    let not_found_404 = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+
     let mut buf = [0; 1024];
     let data_recv_bytes = stream
         .read(&mut buf)
@@ -87,29 +104,39 @@ fn handle_connection(mut stream: TcpStream) {
         .expect("Unable to parse request as &str")
         .parse::<Request>()
         .expect("Unable to parse Request");
-    let response: String = match request.start_line.path.as_str() {
-        "/" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
-        _ if request.start_line.path.starts_with("/echo/") => {
-            let echo = request.start_line.path.strip_prefix("/echo/").unwrap();
-            format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                echo.len(),
-                echo
-            )
-        }
-        "/user-agent" => {
+
+    let path: String = request
+        .start_line
+        .path
+        .split_inclusive('/')
+        .take(2)
+        .collect();
+    let req = (request.start_line.method, path.as_str());
+    let response: String = match req {
+        (HttpMethod::GET, "/") => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+        (HttpMethod::GET, "/user-agent") => {
             let ua = request
                 .headers
                 .get("User-Agent")
                 .expect("Couldnt find User-Agent key in headers map");
             format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                "{}Content-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                ok_200,
                 ua.len(),
                 ua
             )
         }
-        path if request.start_line.path.starts_with("/files/") => {
-            let file_name: String = path
+        (HttpMethod::GET, "/echo/") => {
+            let echo = request.start_line.path.strip_prefix("/echo/").unwrap();
+            format!(
+                "{}Content-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                ok_200,
+                echo.len(),
+                echo
+            )
+        }
+        (HttpMethod::GET, "/files/") => {
+            let file_name: String = request.start_line.path
                 .strip_prefix("/files/")
                 .expect("Couldnt get filename")
                 .to_string();
@@ -121,16 +148,33 @@ fn handle_connection(mut stream: TcpStream) {
 
             if let Ok(content) = std::fs::read_to_string(file_path) {
                 let x = format!(
-                    "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+                    "{}Content-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+                    ok_200,
                     content.len(),
                     content
                 );
                 x
             } else {
-                "HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string()
+                format!("{}", not_found_404)
             }
         }
-        _ => "HTTP/1.1 404 NOT FOUND\r\n\r\n".to_string(),
+        (HttpMethod::POST, "/files/") => {
+            let file_name: String = request.start_line.path
+                .strip_prefix("/files/")
+                .expect("Couldnt get filename")
+                .to_string();
+            let directory: String = std::env::args()
+                .nth(2)
+                .expect("Didnt receive absoulte path")
+                .to_string();
+            let file_path = format!("{}/{}", directory, file_name);
+
+            match std::fs::write(file_path, request.body) {
+                Ok(_) => format!("{}\r\n\r\n", ok_201),
+                Err(_) => format!("{}", not_found_404)
+            }
+        }
+        (_, _) => format!("{}", not_found_404),
     };
 
     stream.write_all(response.as_bytes()).unwrap();
